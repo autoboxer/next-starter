cb13548512fd3a06067a4b9d1d8801c3853acefb
Jared Collier <jared.j.collier@gmail.com>
2023-02-13 11:22:36 -0500

    Update existing code files based on new rules (#5)

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Inter } from '@next/font/google';
 import styles from './page.module.css';
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ['latin']
 });
 
 export default function Home() {
